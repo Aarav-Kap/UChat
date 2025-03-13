dcdb5daca349e7360b89a93a23879b4647a87db3
Aarav-Kap <Aaravthegamerkapoor2@gmail.com>
2025-03-13 21:46:30 +0000

    Remove cookies, fix user count, adjust visuals, restore functionality

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    maxHttpBufferSize: 50 * 1024 * 1024 // Keep 50MB buffer for now, but 10MB limit will reduce strain
+    maxHttpBufferSize: 20 * 1024 * 1024 // Matches 10MB limit + overhead
 });
 
 app.get('/', (req, res) => {
@@ -13,15 +13,24 @@ let userCount = 0;
 
 io.on('connection', (socket) => {
     userCount++;
+    console.log('User connected, userCount:', userCount); // Debug
     io.emit('user count', userCount);
-    console.log('A user connected');
 
     socket.on('chat message', (msg) => {
+        if (!msg || !msg.username || !msg.text && !msg.image) {
+            console.error('Invalid message received:', msg);
+            return;
+        }
+        console.log('Broadcasting chat message from:', msg.username); // Debug
         io.emit('chat message', msg);
     });
 
     socket.on('typing', (username) => {
-        socket.broadcast.emit('typing', username);
+        if (username) {
+            socket.broadcast.emit('typing', username);
+        } else {
+            console.error('No username provided for typing event');
+        }
     });
 
     socket.on('stop typing', () => {
@@ -29,16 +38,33 @@ io.on('connection', (socket) => {
     });
 
     socket.on('name change', (data) => {
-        io.emit('name change', data);
+        if (data && data.oldUsername && data.newUsername) {
+            console.log('Broadcasting name change:', data); // Debug
+            io.emit('name change', data);
+        } else {
+            console.error('Invalid name change data:', data);
+        }
     });
 
     socket.on('disconnect', () => {
         userCount--;
+        console.log('User disconnected, userCount:', userCount); // Debug
         io.emit('user count', userCount);
-        console.log('A user disconnected');
+    });
+
+    socket.on('error', (error) => {
+        console.error('Socket error:', error.message);
     });
 });
 
 http.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log('Server running');
+});
+
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error.message);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
